03748611ce77fc822f90c270259301555e8588a3
Hemant Shori <hemantshori93@gmail.com>
2017-06-06 19:20:36 +0530

    Removed anular-in-memory-api

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
-
 import { AppRoutingModule } from './app-routing.module';
-
-// Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
@@ -20,7 +15,6 @@ import {ProductDetailComponent} from "./product-detail.component";
     BrowserModule,
     FormsModule,
     HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
